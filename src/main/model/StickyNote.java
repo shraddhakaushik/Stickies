24c5e59cbd78426f649ffc6e8f7f2417aa7ea0a4
shraddhakaushik <shraddha.kaushik.g@gmail.com>
2021-02-08 22:31:29 -0800

    Fixed some more bugs

--- src/main/model/StickyNote.java
+++ src/main/model/StickyNote.java
@@ -6,13 +6,16 @@ public class StickyNote {
     private String notes;
 
     public StickyNote(String name, String notes) {
-        this.name = name;
         name = "untitled";
+        notes = "";
+        this.name = name;
         this.notes = notes;
     }
 
     public void assignName(String rename) {
-        name = rename;
+        if (name != rename) {
+            name = rename;
+        }
     }
 
     public void assignNotes(String typed) {
